9b3f269105df2334e9a50551e8d730625d0b41a2
Tom Szymanski <szymanski.tom@gmail.com>
2018-12-03 04:19:03 +0000

    add npm

---
+++ src/components/playerCard.js
@@ -0,0 +1,12 @@
+class PlayerCard extends React.Component {
+    render() {
+        return (
+            <div className="playerCard">
+                <h1>{this.props.name}</h1>
+                <div className="onBase">{this.props.obc}</div>
+                <div className="points">{this.props.points}</div>
+                <div className="avgGameRuns">{this.props.avgGameRuns}</div>
+            </div>
+        );
+    }
+}
