2a5c4faf4b9c04c43bdcd2e11a8739b65582c3f7
Tom Szymanski <szymanski.tom@gmail.com>
2019-02-23 19:05:42 +0000

    a successful real test

--- cypress/integration/page_spec.js
+++ cypress/integration/page_spec.js
@@ -1,5 +1,13 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
+context('Page', () => {
+  beforeEach(() => {
+    cy.visit('http://0.0.0.0:8080/')
+  })
+  describe('Player Cards', function() {
+    it('Displays 25 Cards', function() {
+      cy.get('.playerCardWrapper')
+        .find('.playerCard')
+        .its('length')
+        .should('eq', 25)
     })
   })
+})
