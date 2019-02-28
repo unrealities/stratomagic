a3a41d536d5f95429bf7ff002e000e008b919728
Tom Szymanski <szymanski.tom@gmail.com>
2019-02-28 04:10:16 +0000

    isValidSize tests

--- cypress/integration/unit/game_spec.js
+++ cypress/integration/unit/game_spec.js
@@ -2,9 +2,20 @@ import { Roster } from '../../../src/app/game'
 
 context('Roster', () => {
     describe('constructor', function () {
-        it('accepts players', function () {
+        it('accepts a string', function () {
             let r = new Roster('players')
             expect(r.players).to.equal('players')
+        })
+    })
+
+    describe('isValidSize', function () {
+        it('checks for a zero size', function () {
+            let r = new Roster([])
+            expect(r.isValidSize(0)).to.equal(true)
+        })
+        it('returns false when checking for a non-matching size', function () {
+            let r = new Roster([])
+            expect(r.isValidSize(1)).to.equal(false)
         })
     })
 })
