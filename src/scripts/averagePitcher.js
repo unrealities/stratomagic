2b44ce5a1db55d7b18372ad9ad9264ab7b2847a8
Tom Szymanski <szymanski.tom@gmail.com>
2019-05-03 08:30:56 -0600

    find average joe pitcher

---
+++ src/scripts/averagePitcher.js
@@ -0,0 +1,4 @@
+// TODO
+// Loop through all the players and find the most common pitcher
+// This information is valuable to give hitters a baseline performance metric
+import { PlayersByPosition } from '../data/players';
