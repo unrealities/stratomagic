0fc49b145ed306b2d71a840ec90f3332bddefb8b
Tom Szymanski <szymanski.tom@gmail.com>
2019-05-10 08:42:44 -0600

    start developing simple AI to compete against

---
+++ src/scripts/monteCarlo.js
@@ -0,0 +1,8 @@
+// TODO
+// The idea is to generate random lineups and have them play multiple games against them.
+// The ultimate goal is to figure out the most valuable players/lineups.
+// There are a lot of things to factor and also need to decide how complex the simulation should be.
+// We will need to devise simple basic strategy rules and might want to create certain lineup "types."
+// Players could be grouped into tiers. Could also weight the amount of points spent hitters/sp/rp
+// Will have to exclude strategy cards for any forseeable future.
+// Will need to improve simulation to handle defense and baserunning.
