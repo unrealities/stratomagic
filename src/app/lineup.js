e8a6cfd5a110dde186b3f7faaf465b7721fa8f26
Tom Szymanski <szymanski.tom@gmail.com>
2019-05-16 08:18:38 -0600

    lineupCard and lineup

--- src/app/lineup.js
+++ src/app/lineup.js
@@ -1,19 +1,20 @@
 /*
-    Lineup
+    LineupCard
 
-    A lineup is a subset of a roster. For any given game you set a lineup.
-    Starting Pitcher rotation is a separate discussion, there will be no rules placed on this.
-    A lineup could also be different based on different rules.
+    A LineupCard is a subset of a roster. For any given game you set a lineupCard.
+    A valid lineupCard will depend on the rules of the given game.
 
-    The minimum requirements though are 8 hitters, 1 SP and then a bench of relievers and hitters.
-    We could also have a DH as a 9th hitter and we will assume that as standard for now.
-    We also need to set a batting order.
-
+    The minimum requirements though are 9 hitters (1 may be a SP also if there's no DH), 
+    1 SP and then a bench of relievers and hitters.
+    Players have to bat in a certain batting order (lineup)
+    All positions must be filled by players who can play that position
 */
 
-class Lineup {
-    constructor(hitters, startingPitcher) {
-        this.hitters = hitters;
+class LineupCard {
+    constructor(roster, lineup, startingPitcher, bench) {
+        this.roster = roster;
+        this.lineup = lineup;
         this.startingPitcher = startingPitcher;
+        this.bench = bench;
     }
 }
