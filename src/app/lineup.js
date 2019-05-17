601ce185c59055269734de440007e716d53675ad
Tom Szymanski <szymanski.tom@gmail.com>
2019-05-17 08:24:46 -0600

    lineup validity check

--- src/app/lineup.js
+++ src/app/lineup.js
@@ -18,3 +18,46 @@ class LineupCard {
         this.bench = bench;
     }
 }
+
+/*
+    Lineup
+
+    batting order determines the order in which players hit, looping back to the beginning
+    [] Players
+    all positions in the field must be filled
+    C, 1B, 2B, 3B, SS, LF, CF, RF, P
+*/
+class Lineup {
+    constructor(battingOrder, catcher, firstBase, secondBase, thirdBase, shortstop, leftField, centerField, rightField, pitcher, designatedHitter) {
+        this.battingOrder = battingOrder;
+        this.catcher = catcher;
+        this.firstBase = firstBase;
+        this.secondBase = secondBase;
+        this.thirdBase = thirdBase;
+        this.shortstop = shortstop;
+        this.leftField = leftField;
+        this.centerField = centerField;
+        this.rightField = rightField;
+        this.pitcher = pitcher;
+        this.designatedHitter = designatedHitter;
+    }
+
+    isValid(withDH) {
+        let validWithoutDH =
+            (this.battingOrder.length == 9) &&
+            (this.catcher.playablePositions().includes(2)) &&
+            (this.firstBase.isHitter()) &&
+            (this.secondBase.playablePositions().includes(4)) &&
+            (this.thirdBase.playablePositions().includes(5)) &&
+            (this.shortstop.playablePositions().includes(6)) &&
+            (this.leftField.playablePositions().includes(7)) &&
+            (this.centerField.playablePositions().includes(8)) &&
+            (this.rightField.playablePositions().includes(9)) &&
+            (!this.pitcher.isHitter())
+        if (withDH) {
+            return (this.designatedHitter.playablePositions().length > 0) && validWithoutDH
+        } else {
+            return validWithoutDH
+        }
+    }
+}
