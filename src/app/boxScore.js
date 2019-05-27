8fdc03e0a87297405f24df3b6d39bf277fe99919
Tom Szymanski <szymanski.tom@gmail.com>
2019-05-27 09:28:47 -0600

    Box Score Classes

--- src/app/boxScore.js
+++ src/app/boxScore.js
@@ -12,56 +12,54 @@ export class BoxScore {
 
 class BoxScoreBatter {
     constructor(player) {
+        this.player = player;
         this.ab = 0;
         this.bb = 0;
+        this.double = 0;
+        this.fb = 0;
+        this.gb = 0;
         this.gidp = 0;
         this.hit = 0;
+        this.hr = 0;
         this.lob = 0;
-        this.outcome = { "SO": 0, "PU": 0, "GB": 0, "FB": 0, "BB": 0, "1B": 0, "2B": 0, "3B": 0, "HR": 0 }
+        this.pu = 0;
         this.run = 0;
         this.rbi = 0;
         this.sac = 0;
+        this.single = 0;
         this.so = 0;
         this.totalbase = 0;
-
-
+        this.triple = 0;
     }
 }
 
-/*
-{
-    id: "641778",
-    s: "69",
-    era: "4.73",
-    er: "1",
-    hr: "0",
-    bf: "25",
-    so: "7",
-    bb: "0",
-    r: "1",
-    h: "4",
-    out: "21",
-    note: "(W, 3-4)",
-    s_so: "45",
-    s_bb: "14",
-    s_er: "28",
-    s_r: "30",
-    s_h: "56",
-    s_ip: "53.1",
-    hld: "0",
-    bs: "0",
-    sv: "0",
-    l: "4",
-    w: "3",
-    np: "100",
-    win: "true",
-    pos: "P",
-    name_display_first_last: "Eric Lauer",
-    name: "Lauer",
-}
-*/
 class BoxScorePitcher {
     constructor(player) {
-
+        this.player = player;
+        this.ab = 0;
+        this.bb = 0;
+        this.bf = 0;
+        this.bs = 0;
+        this.double = 0;
+        this.er = 0;
+        this.fb = 0;
+        this.gb = 0;
+        this.gidp = 0;
+        this.hit = 0;
+        this.hold = 0;
+        this.hr = 0;
+        this.lob = 0;
+        this.loss = 0;
+        this.outs = 0;
+        this.pu = 0;
+        this.run = 0;
+        this.rbi = 0;
+        this.sac = 0;
+        this.single = 0;
+        this.so = 0;
+        this.sv = 0;
+        this.totalbase = 0;
+        this.triple = 0;
+        this.win = 0;
     }
 }
