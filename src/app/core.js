2d30a7fe38a22a334bad76d789c49f4dc5995107
Tom Szymanski <szymanski.tom@gmail.com>
2019-07-08 22:02:18 -0500

    getting closer to boxScore

--- src/app/core.js
+++ src/app/core.js
@@ -1,3 +1,4 @@
+import { BattingBoxScoreCardContainer } from '../components/boxScoreCard.js';
 import { LineupCardContainer } from '../components/lineupCard.js';
 
 import React from 'react';
@@ -8,4 +9,6 @@ window.onload = function () {
     let game = newSimulation();
     ReactDOM.render(<LineupCardContainer lineup={game.hLineup}/>, document.getElementById("homeBattingOrder"));
     ReactDOM.render(<LineupCardContainer lineup={game.aLineup}/>, document.getElementById("awayBattingOrder"));
+    ReactDOM.render(<BattingBoxScoreCardContainer boxScore={game.boxScore.aBatters}/>, document.getElementById("awayBattingBoxScore"));
+    ReactDOM.render(<BattingBoxScoreCardContainer boxScore={game.boxScore.hBatters}/>, document.getElementById("homeBattingBoxScore"));
 }
---
+++ src/components/boxScoreCard.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export class BattingBoxScoreCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="battingBoxScoreCard">
+                <div className="name">{this.props.name}</div>
+                <div className="pa">{this.props.pa}</div>
+                <div className="ab">{this.props.ab}</div>
+                <div className="run">{this.props.run}</div>
+                <div className="hit">{this.props.hit}</div>
+                <div className="bb">{this.props.bb}</div>
+                <div className="rbi">{this.props.rbi}</div>
+                <div className="tb">{this.props.tb}</div>
+                <div className="so">{this.props.so}</div>
+                <div className="lob">{this.props.lob}</div>
+            </div>
+        );
+    }
+}
+
+export class PitchingBoxScoreCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="pitchingBoxScoreCard">
+                <div className="name">{this.props.name}</div>
+                <div className="inn">{this.props.inn}</div>
+                <div className="hit">{this.props.hit}</div>
+                <div className="run">{this.props.run}</div>
+                <div className="bb">{this.props.bb}</div>
+                <div className="so">{this.props.so}</div>
+                <div className="hr">{this.props.hr}</div>
+                <div className="tb">{this.props.tb}</div>
+            </div>
+        );
+    }
+}
+
+export class BattingBoxScoreCardContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {cards:[]};
+    }
+
+    componentDidMount(){
+        this.setState({
+            ...this.state,
+            // TODO: Convert this to an array
+            cards: this.props.boxScore,
+        });
+    }
+
+    render() {
+        return(
+            <div className="battingBoxScoreCardContainer">
+                <div className="battingBoxScoreCardWrapper">
+                    {this.state.cards.map( (c,i) => { 
+                        return(
+                            <BattingBoxScoreCard key={i}
+                            name={ c.player.fullName }
+                            pa={ c.pa }
+                            ab={ c.ab }
+                            run={ c.run }
+                            hit={ c.hit }
+                            bb={ c.bb }
+                            rbi={ c.rbi }
+                            tb={ c.tb }
+                            so={ c.so }
+                            lob={ c.lob } />
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
