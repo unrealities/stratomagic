2fafdf0e486bb74e857802270350cd789478103e
Tom Szymanski <szymanski.tom@gmail.com>
2019-07-25 07:13:54 -0500

    sum stats

--- src/app/season.js
+++ src/app/season.js
@@ -11,11 +11,29 @@ export class Season {
     play() {
         this.games.forEach(game => {
             game.playGame();
-            // TODO make logic below actually work
-            battingStats[game.hTeam] += game.boxScore.hBatters;
-            battingStats[game.aTeam] += game.boxScore.aBatters;
-            pitchingStats[game.hTeam] += game.boxScore.hPitchers;
-            pitchingStats[game.aTeam] += game.boxScore.aPitchers;
+            // For every box score element from each game add it to the season stats
+            game.boxScore.hBatters.forEach(batter => {
+                Object.keys(batter).forEach(function(key, _) {
+                    battingStats[game.hTeam][key] += batter[key];
+                });
+            });
+            game.boxScore.aBatters.forEach(batter => {
+                Object.keys(batter).forEach(function(key, _) {
+                    battingStats[game.aTeam][key] += batter[key];
+                });
+            });
+            game.boxScore.hPitchers.forEach(pitcher => {
+                Object.keys(batter).forEach(function(key, _) {
+                    battingStats[game.hTeam][key] += pitcher[key];
+                });
+            });
+            game.boxScore.aPitchers.forEach(pitcher => {
+                Object.keys(batter).forEach(function(key, _) {
+                    battingStats[game.aTeam][key] += pitcher[key];
+                });
+            });
+            console.log(JSON.stringify(this.battingStats));
+            console.log(JSON.stringify(this.pitchingStats));
         });
     }
 }
