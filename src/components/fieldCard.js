dbe323654a73eab65174f2e225b931d8c8a005ff
Tom Szymanski <szymanski.tom@gmail.com>
2019-08-24 07:03:25 -0600

    work towards live game display

---
+++ src/components/fieldCard.js
@@ -0,0 +1,21 @@
+// TODO: 
+// Need to be able to display live game
+// Put defensive players on the field and offensive players on the bases
+import React from 'react';
+
+export class FieldCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="FieldCard">
+                // TODO:
+                // OffenseCard
+                // DefenseCard
+            </div>
+        );
+    }
+}
+
