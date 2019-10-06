8c2ca95b8d4631a87031902378fa1ebb8970a2c1
Tom Szymanski <szymanski.tom@gmail.com>
2019-10-06 13:28:18 -0600

    try copying boxScore in fieldCard

--- src/components/fieldCard.js
+++ src/components/fieldCard.js
@@ -1,7 +1,8 @@
 // Need to be able to display live game
 // Put defensive players on the field and offensive players on the bases
 import React from 'react';
-import {BattingBoxScoreCardContainer} from '../components/boxScoreCard.js';
+// import {BattingBoxScoreCardContainer} from '../components/boxScoreCard.js';
+import { HitterChartCard } from '../components/hitterChartCard.js';
 
 export class FieldCard extends React.Component {
     constructor(props) {
@@ -127,3 +128,151 @@ export class ScoreboardCard extends React.Component {
         )
     }
 }
+
+export class BattingBoxScoreCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="battingBoxScoreCard">
+                <div className="name">
+                    <div className="playerName">{this.props.name}</div>
+                    <HitterChartCard chart={this.props.chart}/>
+                </div>
+                <div className="pa">{this.props.pa}</div>
+                <div className="ab">{this.props.ab}</div>
+                <div className="run">{this.props.run}</div>
+                <div className="hit">{this.props.hit}</div>
+                <div className="bb">{this.props.bb}</div>
+                <div className="rbi">{this.props.rbi}</div>
+                <div className="tb">{this.props.tb}</div>
+                <div className="teamWin">{this.props.teamWin}</div>
+                {/* <div className="so">{this.props.so}</div> */}
+                {/* <div className="lob">{this.props.lob}</div> */}
+                {/* <div className="obp">{this.props.obp}</div> */}
+                <div className="ops">{this.props.ops}</div>
+            </div>
+        );
+    }
+}
+
+export class PitchingBoxScoreCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="pitchingBoxScoreCard">
+                <div className="name">
+                    <div className="playerName">{this.props.name}</div>
+                    <HitterChartCard chart={this.props.chart}/>
+                </div>
+                <div className="inn">{this.props.inn}</div>
+                <div className="hit">{this.props.hit}</div>
+                <div className="run">{this.props.run}</div>
+                <div className="bb">{this.props.bb}</div>
+                <div className="so">{this.props.so}</div>
+                <div className="hr">{this.props.hr}</div>
+                <div className="tb">{this.props.tb}</div>
+                <div className="so">{this.props.so}</div>
+                <div className="teamWin">{this.props.teamWin}</div>
+            </div>
+        );
+    }
+}
+
+export class BattingBoxScoreCardContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {cards:[]};
+    }
+
+    componentDidMount(){
+        let players = Object.values(this.props.boxScore);
+        players = players.filter( function(player) {
+            return player.pa > 0;
+        });
+
+        let header = {'player': {'fullName': 'Batter Name', 'chart': []}, 'pa': 'PA', 'ab': 'AB', 'run': 'R', 'hit': 'H', 'bb': 'BB', 'rbi': 'RBI', 'tb': 'TB', 'teamWin': 'W', 'lob': 'LOB'};
+        players.unshift(header);
+        this.setState({
+            ...this.state,
+            cards: players,
+        });
+    }
+
+    render() {
+        return(
+            <div className="battingBoxScoreCardContainer">
+                <div className="battingBoxScoreCardWrapper">
+                    {this.state.cards.map( (c,i) => { 
+                        return(
+                            <BattingBoxScoreCard key={i}
+                            chart={ c.player.chart }
+                            name={ c.player.fullName }
+                            pa={ c.pa }
+                            ab={ c.ab }
+                            run={ c.run }
+                            hit={ c.hit }
+                            bb={ c.bb }
+                            rbi={ c.rbi }
+                            tb={ c.tb }
+                            so={ c.so }
+                            lob={ c.lob }
+                            teamWin={ c.teamWin } />
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export class PitchingBoxScoreCardContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {cards:[]};
+    }
+
+    componentDidMount(){
+        let players = Object.values(this.props.boxScore);
+        players = players.filter( function(player) {
+            return player.inn > 0;
+        });
+
+        let header = {'player': {'fullName': 'Pitcher Name', 'chart': []}, 'inn': 'INN', 'run': 'ER', 'hit': 'H', 'bb': 'BB', 'hr': 'HR', 'tb': 'TB', 'so': 'K', 'teamWin': 'W'};
+        players.unshift(header);
+        this.setState({
+            ...this.state,
+            cards: players,
+        });
+    }
+
+    render() {
+        return(
+            <div className="pitchingBoxScoreCardContainer">
+                <div className="pitchingBoxScoreCardWrapper">
+                    {this.state.cards.map( (c,i) => { 
+                        return(
+                            <PitchingBoxScoreCard key={i}
+                            chart={ c.player.chart }
+                            name={ c.player.fullName }
+                            inn={ c.inn }
+                            run={ c.run }
+                            hit={ c.hit }
+                            bb={ c.bb }
+                            hr={ c.hr }
+                            tb={ c.tb }
+                            so={ c.so }
+                            teamWin={ c.teamWin } />
+                        )
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
