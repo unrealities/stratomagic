8ad2236af6f8ff791d76fe47524ed14d5323420c
Tom Szymanski <szymanski.tom@gmail.com>
2019-11-22 14:53:59 -0700

    so stylish

--- src/components/fieldCard.js
+++ src/components/fieldCard.js
@@ -40,7 +40,6 @@ export class FieldCard extends React.Component {
                 <ScoreboardCard game={this.props.game}/>
                 <LastPlayCard game={this.props.game}/>
                 <OffenseCard offense={this.props.game.offense}/>
-                <DefenseCard defense={this.props.game.defense}/>
                 <div className="playAtBatButton">
                     <button onClick={this.buttonClick}>Play At Bat</button>
                 </div>
@@ -92,30 +91,6 @@ export class OffenseCard extends React.Component {
                     <div className="batter"><PlayerCard player={this.props.offense.batter}/></div>
                     <div className="onDeck"><PlayerCard player={this.props.offense.onDeck}/></div>
                     <div className="theHole"><PlayerCard player={this.props.offense.theHole}/></div>
-                </div>
-            </div>
-        )
-    }
-}
-
-export class DefenseCard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="defenseCard">
-                <div className="defense">
-                    <div className="pitcher"><PlayerCard player={this.props.defense.pitcher}/></div>
-                    <div className="catcher"><PlayerCard player={this.props.defense.catcher}/></div>
-                    <div className="firstBase"><PlayerCard player={this.props.defense.firstBase}/></div>
-                    <div className="secondBase"><PlayerCard player={this.props.defense.secondBase}/></div>
-                    <div className="thirdBase"><PlayerCard player={this.props.defense.thirdBase}/></div>
-                    <div className="shortstop"><PlayerCard player={this.props.defense.shortstop}/></div>
-                    <div className="leftField"><PlayerCard player={this.props.defense.leftField}/></div>
-                    <div className="centerField"><PlayerCard player={this.props.defense.centerField}/></div>
-                    <div className="rightField"><PlayerCard player={this.props.defense.rightField}/></div>
                 </div>
             </div>
         )
